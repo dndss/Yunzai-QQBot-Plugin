9534f3ac7d9bf64ed143ba46d0ce057720d90cd9
dndss <a20201900@163.com>
2026-05-10 23:16:59 +0800

    移除webhook

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const adapter = new (class QQBotAdapter {
     this.path = "data/QQBot/"
     this.version = "qq-group-bot v1.1.0"
     this.sep = ":" if (process.platform === "win32") this.sep = ""
-    this.appid = {}
     this.converter = new Converter(this)
     this.config = config
   }
@@ -279,33 +278,10 @@ const adapter = new (class QQBotAdapter {
   }
 
   async load() {
-    Bot.express.use(`/${this.name}`, this.makeWebHook.bind(this))
-    Bot.express.quiet.push(`/${this.name}`)
     for (const token of config.token) {
       await connectBot(this, token)
       await Bot.sleep(5000)
     }
-  }
-  makeWebHook(req) {
-    const appid = req.headers["x-bot-appid"]
-    if (!(appid in this.appid)) return Bot.makeLog("warn", "找不到对应 QQBot", appid)
-    if ("plain_token" in req.body?.d)
-      return this.makeWebHookSign(this.appid[appid].uin, req, this.appid[appid].info.secret)
-    if ("t" in req.body) this.appid[appid].sdk.dispatchEvent(req.body.t, req.body)
-    req.res.sendStatus(200)
-  }
-  async makeWebHookSign(id, req, secret) {
-    const { sign } = (await import("tweetnacl")).default
-    const { plain_token, event_ts } = req.body.d
-    while (secret.length < 32) secret = secret.repeat(2).slice(0, 32)
-    const signature = Buffer.from(
-      sign.detached(
-        Buffer.from(`${event_ts}${plain_token}`),
-        sign.keyPair.fromSeed(Buffer.from(secret)).secretKey,
-      ),
-    ).toString("hex")
-    Bot.makeLog("debug", ["QQBot 签名生成", { plain_token, signature }], id)
-    req.res.send({ plain_token, signature })
   }
 })()
 Bot.adapter.push(adapter)
